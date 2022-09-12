1dc54421e1643820925756f925f543701142fd29
Mac <mac@Mac.local>
2022-09-12 11:33:04 +0200

    1

--- src/component/content/BlogDetail.js
+++ src/component/content/BlogDetail.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from '../../customHook/useFetch';
+import Spinner from '../layout/Spinner';
 const BlogDetail = () => {
-  const { id} = useParams();
-    return (
-      <div className='blog-detail'>
-            <h2>Blog Detail - {id}</h2>
-      </div>
-    );
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    loading,
+  } = useFetch('http://localhost:8000/blogs/' + id);
+  return (
+    <div className='blog-detail'>
+      {loading && <Spinner />}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
 };
 
 export default BlogDetail;
